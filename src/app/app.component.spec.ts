101abd5dc2d0bc851a089f09f202e3541f3a3cde
David Kerr <dakerr@outlook.com>
2021-06-30 18:06:27 -0400

    Initial setup
    * bootstrap 5.0.2
    * jest 27.0.4

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -30,6 +30,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('angular-rrweb-auth0 app is running!');
+    expect(compiled.querySelector('a.navbar-brand').textContent).toContain('angular-rrweb-auth0');
   });
 });
